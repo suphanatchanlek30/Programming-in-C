35c62743ef97bbd5e66f266230fa74fff7113b40
suphanatchanlek <suphanatchanlek@gmail.com>
2024-08-29 20:04:14 +0700

    add comment code

--- CS102-HW1-flowcharts-code2.c
+++ CS102-HW1-flowcharts-code2.c
@@ -3,10 +3,11 @@
 int main(void) {
     int Number_integer1, Number_integer2, Number_integer3;
 
-    // Input
+    // Input Number_integer1, Number_integer2, Number_integer3
     printf("Please enter length of each side of your triangle: ");
     scanf("%d %d %d", &Number_integer1, &Number_integer2, &Number_integer3);
 
+    // Calculate
     if(Number_integer1 > 0 && Number_integer2 > 0 && Number_integer3 > 0 && (Number_integer1 + Number_integer2 > Number_integer3) && (Number_integer1 + Number_integer3 > Number_integer2) && (Number_integer2 + Number_integer3 > Number_integer1)){
         if(Number_integer1 == Number_integer2 && Number_integer2 == Number_integer3){
             printf("This is an equilateral triangle triangle.");
@@ -18,7 +19,6 @@ int main(void) {
     } else {
         printf("This is NOT a possible triangle.");
     }
-
 
     return 0;
 }
--- CS102-HW1-flowcharts-code3.c
+++ CS102-HW1-flowcharts-code3.c
@@ -3,14 +3,17 @@
 int main(){
     int Number1, Number2;
 
+    // Input Number1
     printf("Please enter the beginning integer: ");
     scanf("%d",&Number1);
 
+    // Input Number2
     printf("Please enter the last integer: ");
     scanf("%d",&Number2);
 
     printf("All the numbers that can be divided by 13 are: \n");
 
+    // number % 13 == 0
     for(int i=Number1; i<=Number2; i++){
         if(i % 13 == 0){
             printf("%d\n", i);
@@ -18,6 +21,6 @@ int main(){
     }
 
     printf("Bye!!!\n");
-    
+
     return 0;
 }
