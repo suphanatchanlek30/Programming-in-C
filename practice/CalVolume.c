16d100b235aa53b55bfecf88becf735a98c7bde3
suphanatchanlek <suphanatchanlek@gmail.com>
2024-09-24 17:47:16 +0700

    add file CalVolume type file .c and .pdf

---
+++ practice/CalVolume.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int number1, number2, number3;
+
+    scanf("%d %d %d", &number1, &number2, &number3);
+
+    printf("%d\n", number1 * number2 * number3);
+    
+    return 0;
+}
